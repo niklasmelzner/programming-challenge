5e532e7ffb01030af95e04bfd7c939bbd48191b1
niklasmelzner <melznerniklas@gmail.com>
2023-07-01 02:43:49 +0200

    Typ-Konvertierung implementieren

--- src/main/java/de/exxcellent/challenge/datatable/TextColumn.java
+++ src/main/java/de/exxcellent/challenge/datatable/TextColumn.java
@@ -5,6 +5,9 @@ package de.exxcellent.challenge.datatable;
  * Can be converted to a {@link NumericalColumn}
  */
 public class TextColumn extends Column<String> {
+    /**
+     * Buffer für {@link #toNumericalColumn()}, damit die Datenkonvertierung nur einmal durchgeführt wird.
+     **/
     private NumericalColumn numericalColumn;
 
     public TextColumn(String[] values) {
@@ -15,9 +18,18 @@ public class TextColumn extends Column<String> {
      * Converts this {@link Column} to a {@link NumericalColumn}.
      * The copies are independent, changes in the returned object will not reflect in this instance.
      * The method will return the same object on every method call.
+     *
+     * @throws NumberFormatException if at least one value is not convertible to {@link Double}
      */
     public NumericalColumn toNumericalColumn() {
-        throw new UnsupportedOperationException("not implemented");
+        if (numericalColumn == null) {
+            Double[] values = new Double[size()];
+            for (int i = 0; i < size(); i++) {
+                values[i] = Double.valueOf(get(i));
+            }
+            numericalColumn = new NumericalColumn(values);
+        }
+        return numericalColumn;
     }
 }
 
